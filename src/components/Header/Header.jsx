6a3eb7de47e98dfd07675d64d9bcbf35b4ba4243
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-01-26 20:11:10 +0000

    Add styling to the header element Add coder icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from 'react-router-dom';
+import coderIcon from '../../assets/coder.svg';
 import './style.css'
 
 function Header() {
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/projects">Project Gallery</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
-        </nav>
+        <div className="header-container">
+            <img className="navbar-icon" src={coderIcon} />
+            <nav>
+                <ul>
+                    <li><Link to="/" className="navbar-link">Home</Link></li>
+                    <li><Link to="/projects" className="navbar-link">Project Gallery</Link></li>
+                    <li><Link to="/contact" className="navbar-link">Contact</Link></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 

fafa7740829403b3460bd9d1975bc8e9cc4c67dd
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-01-30 12:20:24 +0000

    Add comments to Home page and Project gallery.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+// Import all the required modules and files.
 import React from "react";
 import Header from "../components/Header/Header";
 import IntroductionCard from "../components/IntroductionCard/IntroductionCard";
 import './Home.css'
 import Footer from "../components/Footer/Footer";
 
+// Render the Header, Introduction, and the Footer on the home page.
 function Home() {
 
     return (
--- src/pages/ProjectGallery.jsx
+++ src/pages/ProjectGallery.jsx
@@ -1,3 +1,4 @@
+// Import all the required modules and files.
 import React from "react";
 import { Link } from 'react-router-dom';
 import Header from "../components/Header/Header";
@@ -5,25 +6,27 @@ import Footer from "../components/Footer/Footer";
 import './ProjectGallery.css';
 import projectsData from '../assets/projectData.json'
 
-
 function ProjectGallery() {
-
+    // Dynamically load images from the assets folder
     const imageImports = import.meta.glob('/src/assets/images/*', { eager: true });
 
+    // Function to load an image by filename
     const loadImage = (filename) => {
         const imagePath = `/src/assets/images/${filename}`;
+        // Use dynamic imports to load the image or provide an empty string if not found
         return imageImports[imagePath]?.default || '';
     };
-
 
     return (
         <div>
             <Header />
             <div className="projects-container">
+                {/* Map through project data and create project links */}
                 {projectsData.map(project => (
                     <Link to={`/project/${project.id}`} key={project.id} className="project-link">
                         <div className="project">
                             <h3 className="project-title">{project.title}</h3>
+                            {/* Display project screenshot using the loadImage function */}
                             <img src={loadImage(project.image)} alt={project.title} className="screenshot" />
                         </div>
                     </Link>

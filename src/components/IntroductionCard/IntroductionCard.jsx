c474706f8971cb90596e046ca2b55a48146b0c19
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-01-30 12:08:20 +0000

    Add comments to introduction card on the home page

--- src/components/IntroductionCard/IntroductionCard.jsx
+++ src/components/IntroductionCard/IntroductionCard.jsx
@@ -1,3 +1,4 @@
+// Import all the required modules and files.
 import React from "react";
 import myPhoto from '../../assets/images/myphoto.jpg';
 import htmlIcon from '../../assets/images/html5.png';
@@ -5,18 +6,20 @@ import cssIcon from '../../assets/images/css3.png';
 import javascriptIcon from '../../assets/images/javascript.png';
 import reactIcon from '../../assets/images/react.png';
 import jqueryIcon from '../../assets/images/jquery.png';
-
 import './IntroductionCard.css'
 
+// Introduction card displayed on the home page
 function IntroductionCard() {
     return (
         <div className="intro-card">
             <div className="content-container">
+                {/* Container for introductory text */}
                 <div className="text-container">
                     <h1>Hello I'm Yasser</h1>
                     <p>I'm a passionate developer specializing in Web Development. Excited about creating elegant solutions to complex problems. Welcome to my portfolio, where I showcase my skills and projects.</p>
                     <a href="https://drive.google.com/file/d/1SpHCDTpLAecW9d9eN8MEk37_-PqreD1O/view?usp=drive_link" className="cv-button">View My CV</a>
                 </div>
+                {/* Container for displaying skills icons */}
                 <div className="skills-container">
                     <img src={htmlIcon} alt="HTML5" className="skill-icon" />
                     <img src={cssIcon} alt="CSS3" className="skill-icon" />
@@ -25,6 +28,7 @@ function IntroductionCard() {
                     <img src={jqueryIcon} alt="jQuery" className="skill-icon" />
                 </div>
             </div>
+            {/* Displaying the profile image */}
             <img src={myPhoto} className="profile-image" />
         </div>
     )
